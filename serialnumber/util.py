a3ef8aabfa6ed778532b832596d824c9696782e5
Wagner Macedo <wagnerluis1982@gmail.com>
2015-01-23 10:44:48 -0300

    Correção no parser da NFe para ler mais de um produto

--- serialnumber/util.py
+++ serialnumber/util.py
@@ -7,35 +7,34 @@ def elem(tree, *children, **kwargs):
 
 def iterelem(tree, *children, **kwargs):
     ns = "{%s}" % kwargs.get('ns', "http://www.portalfiscal.inf.br/nfe")
-    return tree.iter('/'.join(["%s%s" % (ns, child) for child in children]))
+    return tree.iterfind('/'.join(["%s%s" % (ns, child) for child in children]))
 
 
 def parse_nfe_document(filename):
     doc = {}
 
     tree = ET.parse(filename)
-    infNFe = elem(tree, "NFe", "infNFe")
+    xinfNFe = elem(tree, "NFe", "infNFe")
 
-    version = float(infNFe.get("versao"))
+    version = float(xinfNFe.get("versao"))
     f = {'id': "ide", 'number': "nNF", 'date': "dhEmi",
          'sup': "emit", 'sup.cnpj': "CNPJ", 'sup.name': "xNome",
-         'sale': "det", 'prod': "prod", 'prod.name': "xProd"}
+         'detail': "det", 'prod': "prod", 'prod.name': "xProd"}
     if version == 3.1:
         pass
     elif version == 2.0:
         f.update({'date': "dEmi"})
 
-    ide = elem(infNFe, f['id'])
-    doc['number'] = int(elem(ide, f['number']).text)
-    doc['date'] = date(*map(int, elem(ide, f['date']).text[0:10].split('-')))
+    xid = elem(xinfNFe, f['id'])
+    doc['number'] = int(elem(xid, f['number']).text)
+    doc['date'] = date(*map(int, elem(xid, f['date']).text[0:10].split('-')))
 
-    emit = elem(infNFe, f['sup'])
-    doc['supplier.cnpj'] = elem(emit, f['sup.cnpj']).text
-    doc['supplier.name'] = elem(emit, f['sup.name']).text
+    xsup = elem(xinfNFe, f['sup'])
+    doc['supplier.cnpj'] = elem(xsup, f['sup.cnpj']).text
+    doc['supplier.name'] = elem(xsup, f['sup.name']).text
 
-    det = elem(infNFe, f['sale'])
     doc['products'] = products = []
-    for prod in iterelem(det, f['prod']):
-        products.append(elem(prod, f['prod.name']).text)
+    for xprod in iterelem(xinfNFe, f['detail'], f['prod'], f['prod.name']):
+        products.append(xprod.text)
 
     return doc
